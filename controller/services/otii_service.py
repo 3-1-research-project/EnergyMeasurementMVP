c42556247e30a127cf2908af4a19a3fd5bedd065
Malou Landsgaard <elmelund@hotmail.dk>
2024-12-04 10:43:49 +0000

    csv writer

--- controller/services/otii_service.py
+++ controller/services/otii_service.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 import json
 from otii_tcp_client import otii_connection, otii as otii_application
+from otii_tcp_client import arc as otii_arc
 
 
 class OtiiService:
@@ -48,8 +49,14 @@ class OtiiService:
 
         return project, device
 
-    def collect_data(self, otii_project, device, schema_path):
+    def collect_data(
+        self,
+        otii_project: otii_application.project.Project,
+        device: otii_arc.Arc,
+        schema_path: str,
+    ):
         # Get statistics for the recording
+
         recording = otii_project.get_last_recording()
         # info = recording.get_channel_info(device.id, "mc")
         # statistics = recording.get_channel_statistics(
@@ -60,58 +67,44 @@ class OtiiService:
         voltage_count = recording.get_channel_data_count(device.id, "mv")
         power_count = recording.get_channel_data_count(device.id, "mp")
 
-        current_data = recording.get_channel_data(device.id, "mc", 0, current_count)
-        voltage_data = recording.get_channel_data(device.id, "mv", 0, voltage_count)
-        power_data = recording.get_channel_data(device.id, "mp", 0, power_count)
-
-        print("data count")
-        print("current: " + str(current_count))
-        print("voltage: " + str(voltage_count))
-        print("power: " + str(power_count))
-
-        data = {}
-        data["current"] = current_data
-        data["voltage"] = voltage_data
-        data["power"] = power_data
-        data["time"] = [
-            i / 10000 for i in range(current_count)
-        ]  # 10000 is the sample rate
-
-        # Get the current timestamp
+        # Create a CSV file
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
         schema = schema_path.split("/")[-1].split(".")[0]
+        filename = f"{schema}_{timestamp}.csv"
 
-        # Create the filename with the timestamp
-        filename = f"{schema}_{timestamp}.json"
+        with open(filename, mode="w") as data_file:
+            # Header
+            data_writer = csv.writer(
+                data_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+            )
+            data_writer.writerow(["seconds", "current", "voltage", "power"])
 
-        # Write the nested data to a JSON file
-        with open(filename, "w") as f:
-            json.dump(data, f, indent=4)  # Use indent for pretty formatting
+            # Write data
+            print("Writing data to CSV...")
 
-        # Print the statistics
-        #    print(f'From:        {info["from"]} s')
-        #    print(f'To:          {info["to"]} s')
-        #    print(f'Offset:      {info["offset"]} s')
-        #    print(f'Sample rate: {info["sample_rate"]}')
+            index = 0
+            while index < current_count:
+                current_data = recording.get_channel_data(device.id, "mc", index, 1000)[
+                    "values"
+                ]
+                voltage_data = recording.get_channel_data(device.id, "mv", index, 1000)[
+                    "values"
+                ]
+                power_data = recording.get_channel_data(device.id, "mp", index, 1000)[
+                    "values"
+                ]
 
-        #     print(f'Min:         {statistics["min"]:.5} A')
-        #     print(f'Max:         {statistics["max"]:.5} A')
-        #     print(f'Average:     {statistics["average"]:.5} A')
-        #     print(f'Energy:      {statistics["energy"] / 3600:.5} Wh')
+                # add to csv
+                for i in range(1000):
+                    data_writer.writerow(
+                        [
+                            (index + i) / 10000,
+                            current_data[i],
+                            voltage_data[i],
+                            power_data[i],
+                        ]
+                    )
 
-        # def generate_output(otii_project, device):
-        #     # Get statistics for the recording
-        #     recording = otii_project.get_last_recording()
-        #     minimum, maximum, avg, energy = recording.get_complete_channel_statistics(device, Channel.MAIN_CURRENT)
-        #     print(f"{Channel.MAIN_CURRENT.name}: {minimum}, {maximum}, {avg}, {energy}", flush=True)
+                index += 1000
 
-        #     for channel in (Channel.MAIN_VOLTAGE, Channel.MAIN_POWER):
-        #         minimum, maximum, avg = recording.get_complete_channel_statistics(device, channel)
-        #         print(f"{channel.name}: {minimum}, {maximum}, {avg}", flush=True)
-
-        # async def get_async(url):
-        #     print(f"Starting scenario on {url}...", flush=True)
-        #     timeout = httpx.Timeout(10.0, read=None)
-        #     async with httpx.AsyncClient() as client:
-        #         return await client.get(url, timeout=timeout)
+        print(f"Data saved to {filename}!")
